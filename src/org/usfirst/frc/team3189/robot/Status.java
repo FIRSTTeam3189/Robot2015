8517ab4071649b98f39964b0050e1e73b99d85f9
Quest Osucha <combineturret@gmail.com>
2015-01-05 16:24:00 -0800

    +Added gyro angle to SmartDashboard

--- src/org/usfirst/frc/team3189/robot/Status.java
+++ src/org/usfirst/frc/team3189/robot/Status.java
@@ -4,8 +4,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Status {
 	
-	public static final String TEST_AI_KEY = "Test Analog Input";
-	public static final String TEST_DI_KEY = "Test Digital Input";
+	public static final String TEST_AI_KEY    = "Test Analog Input";
+	public static final String TEST_DI_KEY    = "Test Digital Input";
+	public static final String GYRO_ANGLE_KEY = "Gyro Angle";
 	
 	public static void initStatus () {
 		
@@ -14,5 +15,6 @@ public class Status {
 	public static void updateStatus () {
 		SmartDashboard.putBoolean(TEST_DI_KEY, Robot.test.getTestDI());
 		SmartDashboard.putNumber(TEST_AI_KEY, Robot.test.getTestAI());
+		SmartDashboard.putNumber(GYRO_ANGLE_KEY, Robot.lateralDrivetrain.getGyroAngle());
 	}
 }
