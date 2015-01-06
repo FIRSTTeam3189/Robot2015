3944d02e15b4d0befe016244ca084b82fd80216b
mcprog <michaelcurtis96@gmail.com>
2015-01-05 17:47:01 -0800

    Started on arcade drive Committing to switch over to Kalen's computer
    -Michael Curtis

--- src/org/usfirst/frc/team3189/robot/subsystems/LongDrivetrain.java
+++ src/org/usfirst/frc/team3189/robot/subsystems/LongDrivetrain.java
@@ -1,9 +1,11 @@
 
 package org.usfirst.frc.team3189.robot.subsystems;
 
+import org.usfirst.frc.team3189.robot.Robot;
 import org.usfirst.frc.team3189.robot.RobotMap;
 import org.usfirst.frc.team3189.robot.commands.TankDriveCommand;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -17,10 +19,12 @@ public class LongDrivetrain extends Subsystem {
     // here. Call these from Commands.
 	private SpeedController left;
 	private SpeedController right;
+	private RobotDrive drive;
 	
 	public LongDrivetrain() {
 		left = new Talon(RobotMap.leftMotor);
 		right = new Talon(RobotMap.rightMotor);
+		drive = new RobotDrive(RobotMap.leftMotor, RobotMap.rightMotor);
 	}
 
     public void initDefaultCommand() {
@@ -30,6 +34,7 @@ public class LongDrivetrain extends Subsystem {
     public void move (double leftPwr, double rightPwr) {
     	left.set(leftPwr);
     	right.set(rightPwr);
+    	drive.arcadeDrive(Robot.oi, true);
     }
     
     public void kill () {
