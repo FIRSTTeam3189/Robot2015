93c9f826774165018d6d82ede9efb1f51e24366b
mcprog <michaelcurtis96@gmail.com>
2015-02-09 16:45:13 -0800

    added logic to center elevator iff it starts at lower state

--- src/org/usfirst/frc/team3189/robot/subsystems/Winch.java
+++ src/org/usfirst/frc/team3189/robot/subsystems/Winch.java
@@ -2,11 +2,13 @@ package org.usfirst.frc.team3189.robot.subsystems;
 
 import org.usfirst.frc.team3189.robot.RobotMap;
 import org.usfirst.frc.team3189.robot.commands.WinchDoNothing;
+import org.usfirst.frc.team3189.robot.utility.Variables;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -18,12 +20,16 @@ public class Winch extends Subsystem {
     public DigitalInput lowerIR;
     public DigitalInput upperIR;
     public DigitalInput winchLimit;
+    private Timer elevatorTimer;
+    private boolean hasTime;
+    private double halfTime;
     
     public Winch() {
     	motor = new Victor(RobotMap.winchMotor);
     	lowerIR = new DigitalInput(RobotMap.winchLowerIRChannel);
     	upperIR = new DigitalInput(RobotMap.winchUpperIRChannel);
     	winchLimit = new DigitalInput(RobotMap.winchLimit);
+    	elevatorTimer = new Timer();
     }
     
     public void initDefaultCommand() {
@@ -35,6 +41,27 @@ public class Winch extends Subsystem {
     		kill();
     	else
     		motor.set(speed);
+    }
+    
+    public void moveToIRMedium () {
+    	if (lowerIRState() && elevatorTimer.get() == 0) {
+    		setSpeed(Variables.winchUpSpeed.get());
+    		elevatorTimer.start();
+    	}
+    	else if (upperIRState() && elevatorTimer.get() > 0 && !hasTime) {
+    		setSpeed(Variables.winchDownSpeed.get());
+    		elevatorTimer.stop();
+    		halfTime =  elevatorTimer.get();
+    		elevatorTimer.reset();
+    		hasTime = true;
+    	}
+    	else if (lowerIRState() && elevatorTimer.get() == 0 && hasTime) {
+    		setSpeed(Variables.winchUpSpeed.get());
+    		elevatorTimer.start();
+    	}
+    	else if (elevatorTimer.get() >= halfTime) {
+    		setSpeed(0);
+    	}
     }
     
     public void kill() {
