063940893571e8b25a90a2c442d3b213b4050ab3
Jay Rutten <jrutten@pacbell.net>
2015-02-15 13:28:10 -0800

    Cleaned up unused imports.

--- src/org/usfirst/frc/team3189/robot/commands/auto/WinchUpLevel.java
+++ src/org/usfirst/frc/team3189/robot/commands/auto/WinchUpLevel.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team3189.robot.commands.auto;
 
 import org.usfirst.frc.team3189.robot.Robot;
-import org.usfirst.frc.team3189.robot.utility.Variables;
 
 import edu.wpi.first.wpilibj.command.Command;
 
